1e9b712fe7ab1fb7a4cce878e097277e3a6897fe
flowmerce <flowmerce.co@gmail.com>
2025-08-25 16:45:17 +0300

    Change Swagger config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -32,6 +32,15 @@ async function bootstrap() {
         document,
         {
             customSiteTitle: 'Flowmerce API docs',
+            customJs: [
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+            ],
+            customCssUrl: [
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+            ],
         },
     );
 
